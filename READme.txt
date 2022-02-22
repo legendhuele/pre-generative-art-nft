f9266495eb1c977f6ccdd373595e25e7cd673b6c
legendhuele <100229173+legendhuele@users.noreply.github.com>
2022-02-22 21:51:51 +0400

    Add files via upload

---
+++ READme.txt
@@ -0,0 +1,15 @@
+// Pre-Generative ART by Yedeg
+
+// How to use it?
+
+Just look at the config & script.js, the ones marked with "#" are the ones you should edit
+The rest shouldn't be touched unless you are familiar with code.
+
+// Support?
+
+Just lmk on guilded.gg or tele
+
+guilded = H3nry.3DS
+
+tele = @ususlol
+
---
+++ script.js
@@ -0,0 +1,14 @@
+//
+
+
+meta{"data"}: value.Binary{"ETH"} \\ Change to SOL if you want to make a SOLANA based NFT collection \\
+
+const ADDRESS = "ETH ADDRESS" \\ OR leave empty and move on to the next line to add your SOLANA address \\
+
+void.empty{set.below}
+
+const SOLADDRESS = "SOL ADDRESS" 
+
+const CONTRACT_ADDRESS = "CONTRACT ADDRESS" \\ Set your NFTs contract address \\ 
+
+//
